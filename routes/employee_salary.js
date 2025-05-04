30bbae681b99f5dd6f9980a982b6d8e5c84ac041
Khusham Bansal <kbkhushambansal@gmail.com>
2025-05-04 11:15:13 +0530

    Fixed Employee files

--- routes/employee_salary.js
+++ routes/employee_salary.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const { 
   createEmployeeSalary,
-  getAllEmployeeSalaries,
-  getEmployeeSalaryByJobType,
-  updateEmployeeSalary,
-  deleteEmployeeSalary
+  // getAllEmployeeSalaries,
+  // getEmployeeSalaryByJobType,
+  // updateEmployeeSalary,
+  // deleteEmployeeSalary
 } = require('../controllers/employee/employeeSalaryController');
 
 const router = express.Router();
 
 // Employee salary routes
 router.post('/create', createEmployeeSalary);
-router.get('/', getAllEmployeeSalaries);
-router.get('/:jobtype', getEmployeeSalaryByJobType);
-router.put('/:jobtype', updateEmployeeSalary);
-router.delete('/:jobtype', deleteEmployeeSalary);
+// router.get('/', getAllEmployeeSalaries);
+// router.get('/:jobtype', getEmployeeSalaryByJobType);
+// router.put('/:jobtype', updateEmployeeSalary);
+// router.delete('/:jobtype', deleteEmployeeSalary);
 
 module.exports = router;

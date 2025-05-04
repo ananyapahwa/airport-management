255a3582e435d53b304c96dfb84b61b05b157d7c
ananyapahwa8 <ananya.pahwa@medlr.in>
2025-05-04 22:36:07 +0530

    route name change

--- routes/employee_details.js
+++ routes/employee_details.js
@@ -5,6 +5,6 @@ const {
 
 const router = express.Router();
 
-router.post('/create', createEmployeeDetails);
+router.post('/create-employee-details', createEmployeeDetails);
 
 module.exports = router;
